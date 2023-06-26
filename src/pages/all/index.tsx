de1ad509ee06b62e47b093b10502f146d99f1a29
gracefullight <gracefullight.dev@gmail.com>
2023-06-27 03:36:35 +0900

    feat: ✨ 전체 탭 추가

--- src/pages/all/index.tsx
+++ src/pages/all/index.tsx
@@ -1,3 +1,4 @@
+import { Cog6ToothIcon, MagnifyingGlassIcon } from "@heroicons/react/24/solid";
 import type { GetServerSidePropsContext } from "next";
 import Head from "next/head";
 import { Layout } from "~/components/layout";
@@ -10,7 +11,19 @@ export default function All() {
       <Head>
         <title>Doss | 전체</title>
       </Head>
-      <span>전체</span>
+      <div className="flex w-screen flex-col gap-4 bg-neutral-800 px-6 pt-4">
+        <div className="flex justify-end">
+          <button className="btn-ghost btn-square btn">
+            <MagnifyingGlassIcon className="w-6" />
+          </button>
+          <button className="btn-ghost btn-square btn">
+            <Cog6ToothIcon className="w-6" />
+          </button>
+        </div>
+        <div className="mb-4 flex items-center gap-4">
+          <h1 className="text-2xl font-bold text-neutral-200">전체</h1>
+        </div>
+      </div>
     </Layout>
   );
 }
