cc8f7f03b3a21bcacba80da11e8b4f7733f4831f
gracefullight <gracefullight.dev@gmail.com>
2023-07-23 10:51:11 +0900

    feat: ✨ nearby layout

--- src/pages/benefit/nearby/index.tsx
+++ src/pages/benefit/nearby/index.tsx
@@ -1,5 +1,7 @@
+import { ChevronRightIcon, FaceSmileIcon } from "@heroicons/react/24/solid";
 import Head from "next/head";
 import { StackLayout, type StackLayoutNavbarItem } from "~/components/layout";
+import { formatNumber } from "~/utils/number";
 
 export default function BenefitNearby() {
   const navbarItems: StackLayoutNavbarItem[] = [
@@ -18,7 +20,21 @@ export default function BenefitNearby() {
       <Head>
         <title>Doss | 혜택 | 토스켜기</title>
       </Head>
-      <StackLayout items={navbarItems}>11</StackLayout>
+      <StackLayout items={navbarItems}>
+        <div className="flex-grow bg-neutral-800">Main content area.</div>
+        <div className="flex cursor-pointer items-center justify-between px-6 py-6">
+          <div className="flex gap-2">
+            <FaceSmileIcon className="w-10 fill-warning" />
+            <div className="flex flex-col gap-1">
+              <span className="text-sm">
+                함께 도스 켜고 총 {formatNumber(259)}원 모았어요
+              </span>
+              <span className="font-medium text-info">내 캐릭터 보기</span>
+            </div>
+          </div>
+          <ChevronRightIcon className="w-4" />
+        </div>
+      </StackLayout>
     </>
   );
 }
