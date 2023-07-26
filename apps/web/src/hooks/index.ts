1bf15a2b1fe6acd484b66568f032709280b7c02d
gracefullight <gracefullight.dev@gmail.com>
2023-07-26 16:23:17 +0900

    feat: ✨ useReadClipboard

--- apps/web/src/hooks/index.ts
+++ apps/web/src/hooks/index.ts
@@ -1,3 +1,3 @@
 export { default as useGeolocation } from "./useGeolocation";
 export { default as usePedometer } from "./usePedometer";
-
+export { default as useReadClipboard } from "./useReadClipboard";
---
+++ apps/web/src/hooks/useReadClipboard.ts
@@ -0,0 +1,25 @@
+import { useEffect, useState } from "react";
+
+export default function useReadClipboard() {
+  const [value, setValue] = useState<string | null>(null);
+  const [permission, setPermission] = useState<string>("");
+
+  useEffect(() => {
+    // ? https://github.com/microsoft/TypeScript-DOM-lib-generator/issues/1245
+    navigator.permissions
+      .query({ name: "clipboard-read" as PermissionName })
+      .then((result) => {
+        setPermission(result.state);
+        if (result.state === "granted" || result.state === "prompt") {
+          navigator.clipboard.readText().then(
+            (clipText) => setValue(clipText),
+            () => setValue(null),
+          );
+        } else {
+          setValue(null);
+        }
+      });
+  }, []);
+
+  return { permission, value };
+}
