18aa4335a5a217e013d4c29001dc8eb8aad9a83a
gracefullight <gracefullight.dev@gmail.com>
2023-08-13 11:47:55 +0900

    chore: 🔧 remove example router

--- apps/web/src/server/api/root.ts
+++ apps/web/src/server/api/root.ts
@@ -1,4 +1,4 @@
-import { exampleRouter, mainRouter } from "~/server/api/routers";
+import { mainRouter } from "~/server/api/routers";
 import { createTRPCRouter } from "~/server/api/trpc";
 
 /**
@@ -7,7 +7,6 @@ import { createTRPCRouter } from "~/server/api/trpc";
  * All routers added in /api/routers should be manually added here.
  */
 export const appRouter = createTRPCRouter({
-  example: exampleRouter,
   main: mainRouter,
 });
 
