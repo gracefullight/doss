79211c2e579b37693785233d7cbb8456f8b58c68
gracefullight <gracefullight.dev@gmail.com>
2023-09-19 23:27:39 +0900

    feat: ✨ sentry

---
+++ apps/web/sentry.server.config.ts
@@ -0,0 +1,15 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://1b38e3c7e2914e8300a5ce1fcd681d33@o4504563242434560.ingest.sentry.io/4505907560054784",
+
+  // Adjust this value in production, or use tracesSampler for greater control
+  tracesSampleRate: 1,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+});
