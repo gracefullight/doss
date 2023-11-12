594a343a78e66ded1e72b42da47bad62803faec5
gracefullight <gracefullight.dev@gmail.com>
2023-11-12 12:45:51 +0900

    fix: 🐛 viewport

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -29,6 +29,10 @@ export const metadata: Metadata = {
 };
 
 export const viewport: Viewport = {
+  width: "device-width",
+  initialScale: 1,
+  maximumScale: 1,
+  colorScheme: "dark",
   themeColor: "#333333",
 };
 
