eda1a0ad00ab609dfd868ca306edb6be7d265eb9
gracefullight <gracefullight.dev@gmail.com>
2024-03-16 18:37:05 +0900

    fix: 🐛 disable openapi.json

--- apps/web/src/app/api/openapi.json/route.ts
+++ apps/web/src/app/api/openapi.json/route.ts
@@ -1,12 +1,17 @@
 import type { NextRequest } from "next/server";
-
 import { NextResponse } from "next/server";
-import { openApiDocument } from "~/server/openapi";
 
-// Respond with our OpenAPI schema
+// import { NextResponse } from "next/server";
+// import { openApiDocument } from "~/server/openapi";
+
+// // Respond with our OpenAPI schema
+// const handler = (_: NextRequest) => {
+//   // ? https://nextjs.org/docs/app/api-reference/functions/next-response#json
+//   return NextResponse.json(openApiDocument);
+// };
+
 const handler = (_: NextRequest) => {
-  // ? https://nextjs.org/docs/app/api-reference/functions/next-response#json
-  return NextResponse.json(openApiDocument);
+  return NextResponse.json({});
 };
 
 export { handler as GET };
