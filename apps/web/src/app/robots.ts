e741321f7f855c2e31ac81557729e1166eeede1d
gracefullight <gracefullight.dev@gmail.com>
2024-04-20 15:48:28 +0900

    fix(web): 🐛 disable auth path

--- apps/web/src/app/robots.ts
+++ apps/web/src/app/robots.ts
@@ -2,10 +2,12 @@ import type { MetadataRoute } from "next";
 
 export default function robots(): MetadataRoute.Robots {
   return {
-    rules: {
-      userAgent: "*",
-      allow: "/",
-    },
+    rules: [
+      {
+        userAgent: "*",
+        disallow: "/auth",
+      },
+    ],
     sitemap: "https://doss.gracefullight.dev/sitemap.xml",
   };
 }
