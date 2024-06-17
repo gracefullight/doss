ec7a19967736826ba360a65e3918150fdcdca2b0
gracefullight <gracefullight.dev@gmail.com>
2024-06-18 06:28:41 +0900

    feat: ✨ ref pnpm-workspace.yaml

--- vitest.workspace.ts
+++ vitest.workspace.ts
@@ -1,3 +1,24 @@
+import { readFileSync } from "node:fs";
+import { resolve } from "node:path";
+
 import { defineWorkspace } from "vitest/config";
 
-export default defineWorkspace(["apps/*", "packages/*"]);
+const workspaceFilePath = resolve(__dirname, "pnpm-workspace.yaml");
+
+function getPackagesFromWorkspace(): string[] {
+  const fileContents = readFileSync(workspaceFilePath, "utf8");
+
+  const packagePattern = /-\s*'(.*?)'/g;
+  const matches = [...fileContents.matchAll(packagePattern)];
+
+  if (!matches) {
+    throw new Error("packages field not found in pnpm-workspace.yaml");
+  }
+
+  const packages = matches.map((match) => match[1]);
+  return packages;
+}
+
+const packages = getPackagesFromWorkspace();
+
+export default defineWorkspace(packages);
